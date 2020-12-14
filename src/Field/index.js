068cf2df303955cf259e96d12f776d9fe0f5c6b5
Nick Hough <nick@portonefive.com>
2020-12-14 10:08:54 -0700

    Consider removing a few of the Field props.

--- src/Field/index.js
+++ src/Field/index.js
@@ -3,21 +3,23 @@ export default class Field {
     this.key = config.key || '';
     this.arrayOf = config.arrayOf || null;
     this.default = config.default !== undefined ? config.default : null;
-    this.formFieldType = config.formFieldType || 'text';
-    this.handler = config.handler || null;
     this.help = config.help || '';
     this.hidden = config.hidden || false;
     this.label = config.label || '';
     this.max = config.max || null;
     this.min = config.min || null;
     this.multiple = config.multiple || false;
-    this.options = config.options ||  [];
-    this.payloadKey = config.payloadKey || this.key;
+    this.options = config.options || [];
     this.placeholder = config.placeholder || '';
-    this.render = config.render || null;
     this.required = config.required || false;
     this.rows = config.rows || 2;
     this.step = config.step || null;
     this.type =  config.type || null;
+
+    // Too project specific? Should these get extracted?
+    this.payloadKey = config.payloadKey || this.key;
+    this.formFieldType = config.formFieldType || 'text';
+    this.handler = config.handler || null;
+    this.render = config.render || null;
   }
 }
