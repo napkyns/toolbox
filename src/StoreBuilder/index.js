b1acd8a65c03045795b91bbde569b68de9d76919
Nick Hough <nick@portonefive.com>
2020-12-18 10:50:29 -0700

    Added Redux integration to StoreBuilder.

--- src/StoreBuilder/index.js
+++ src/StoreBuilder/index.js
@@ -27,4 +27,92 @@ export default class StoreBuilder {
   getVuexModules() {
     return this.modules;
   }
+
+  getReduxModules() {
+    const modules = {};
+
+    for (const [key, value] of Object.entries(this.modules)) {
+      modules[key] = this.vuexModuleToReduxModule(value);
+    }
+
+    return modules;
+  }
+
+  getReduxReducers() {
+    const reducers = {};
+
+    for (const [key, value] of Object.entries(this.getReduxModules())) {
+      reducers[key] = value.reducer;
+    }
+
+    return reducers;
+  }
+
+  vuexModuleToReduxModule(vuexModule) {
+
+    const defaultState = {
+      ...vuexModule.state,
+    };
+
+    if (!vuexModule.mutations) {
+      vuexModule.mutations = {};
+    }
+
+    if (!vuexModule.actions) {
+      vuexModule.actions = {};
+    }
+
+    const actions = {};
+
+    for (let [actionKey, action] of Object.entries(vuexModule.actions)) {
+
+      actions[actionKey] = (payload) => {
+
+        return (dispatch, getState) => {
+
+          const commit = (key, payload) => {
+            return dispatch({
+              type: key,
+              payload,
+            });
+          };
+
+          const context = {
+            commit,
+            state: getState()[vuexModule.key],
+          };
+
+          return action(context, payload);
+        };
+      };
+    }
+
+    if (!vuexModule.key) {
+      console.error('Missing Vuex Module Key');
+    }
+
+    return {
+
+      key: vuexModule.key,
+
+      actions,
+
+      defaultState,
+
+      reducer: (state = defaultState, action) => {
+
+        if (action.type && vuexModule.mutations.hasOwnProperty(action.type)) {
+
+          const mutation = vuexModule.mutations[action.type];
+          mutation(state, action.payload);
+
+          return {
+            ...state,
+          };
+        }
+
+        return state;
+      },
+    };
+  }
 }
